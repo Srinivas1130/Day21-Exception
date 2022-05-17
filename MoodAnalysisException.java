76c14ac829a2d754715c22e323c89ed37153a39d
Srinivas1130 <srinivasachar1998@gmail.com>
2022-05-17 09:21:18 +0530

    Added code for exception for mood analyser

---
+++ MoodAnalysisException.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day21;
+
+public class MoodAnalysisException extends Exception {
+
+	String message;
+
+	public MoodAnalysisException(String message) {
+		super(message);
+	}
+}
